194ee0935246470552a96d5a153ba2eae9634c71
Eduardo Decarli <decarliedu@gmail.com>
2024-10-01 09:12:14 -0300

    refactory: reshaping the project organization

---
+++ src/main/java/dao/ConnectionFactory.java
@@ -0,0 +1,10 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    private static final String URL = "";
+    private static final String USER = "";
+    private static final String PASSWORD = "";
+}
